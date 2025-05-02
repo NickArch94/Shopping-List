5049cdc571b71c7503ba06feb2b29887ab8a7160
NickArch94 <158612778+NickArch94@users.noreply.github.com>
2025-05-02 10:07:38 -0600

    Made final adjustments to the 'Script.js' file, and added some more styling to the CSS script as well.

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ function initShoppingList() {
     });
 }
 
-function handleItemForm(event) {
+function handleItemForm(event, formRef) {
     if(event.preventDefault) {
         event.preventDefault();
     }
     addItemToShoppingList();
+    formRef.reset();
 
     return false;
 }
@@ -22,15 +23,36 @@ function handleItemForm(event) {
 function addItemToShoppingList() {
     let itemName = document.getElementById("item-name");
     let itemAmount = document.getElementById("item-amount");
+    let id = getRandomInt(0, 10000000);
 
-    let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+    let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
     let itemListRef = document.getElementById("shopping-list");
     itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+    setDeleteButtonEvent(id);
+}
+function setDeleteButtonEvent(id) {
+    let deleteButton = document.getElementById("button"+id);
+    deleteButton.addEventListener("click", () => {
+        removeListItem(id);
+    });
 }
 
-function createListItemHtml(itemName, itemAmount) {
-    return `<li>
+function removeListItem(id) {
+    let listItem = document.getElementById("item"+id);
+    listItem.parentNode.removeChild(listItem);
+}
+
+function createListItemHtml(itemName, itemAmount, id) {
+    return `<li id="item${id}">
                ${itemName} - ${itemAmount}
+               <button id="button${id}" type="button">Delete Item</button>
             </li>
     `;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
